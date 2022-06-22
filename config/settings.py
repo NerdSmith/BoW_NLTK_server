4c3c4d1d69e361fab81daa599c40856fa9b7c579
Nikita Merzlyakov <merzlyakov02@gmail.com>
2022-06-22 23:40:36 +0300

    db config

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from environs import Env # new
+env = Env() # new
+env.read_env() # new
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -82,11 +87,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+ "default": env.dj_db_url("DATABASE_URL")
 }
 
 
